06b8146c4e2536f161ca1f2ed663983446d402a4
Sheldon Lai <ie925155@gmail.com>
2016-05-09 22:54:21 +0800

    Avoid header redifinition

--- symbols.h
+++ symbols.h
@@ -6,5 +6,11 @@
  * set the header up to work correctly even if included more than once.
  */
 
+#ifndef _symbols_h
+#define _symbols_h
+
 void PrintSymtab(const char *filename);
 void SearchSymbol(const char *filename, const char *address);
+
+
+#endif //end of _symbols_h
